49ea8f324f8f1a20220f6b33c2b53f0311c89bb1
Drew Page <drew@siegemedia.com>
2020-03-07 14:35:58 -0800

    Added GraphQL folder with inital typedefs and resolvers

---
+++ server/src/graphql/typeDefs.ts
@@ -0,0 +1,18 @@
+import { gql } from 'apollo-server-express';
+
+export const typeDefs = gql`
+  type Quote {
+    id: ID!
+    quote: String!
+    author: String!
+    category: String!
+    period: Int!
+    image: String!
+  },
+  type Query {
+    quotes: [Quote!]!
+  },
+  type Mutation {
+    deleteQuote(id: ID!): Quote!
+  }
+`;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,28 +1,14 @@
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs, resolvers } from './graphql';
 import { quotes } from './quotes';
 import bodyParser from 'body-parser';
 
 const app = express();
+const server = new ApolloServer({ typeDefs, resolvers });
 const port = 9000;
 
-const one: number = 1;
-const two: number = 2;
-
-app.get('/', (_req, res) => res.send(`1 + 2 = ${one + two}`));
-app.get('/quotes', (_req, res) => res.send(quotes));
-
-app.use(bodyParser.json());
-
-app.post('/delete-quote', (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < quotes.length; i++) {
-    if (quotes[i].id === id) {
-      return res.send(quotes.splice(i, 1)[0]);
-    }
-  }
-  return res.send("Failed to delete quote.");
-});
+server.applyMiddleware({app, path: '/api'});
 
 app.listen(port);
 
