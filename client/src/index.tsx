58a95621d83cd3bcb56002d1dd2566bc27dd2394
Drew Page <drew@siegemedia.com>
2020-03-09 16:33:57 -0700

    updated the sections and exports for react router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './styles/index.css';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import './styles/index.css';
-import { Quotes } from './sections/Quotes';
+import { Home, Create, Quote, Quotes, NotFound, User } from './sections';
 import * as serviceWorker from './serviceWorker';
 
 const client = new ApolloClient({
   uri: '/api'
 });
 
+const App = () => {
+  return (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/create" component={Create} />
+      <Route exact path="/quote/:id" component={Quote} />
+      <Route exact path="/quotes/:category?" component={Quotes} />
+      <Route exact path="/user/:id" component={User} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>
+  );
+}
+
 ReactDOM.render(
 <ApolloProvider client={client}>
-  <Quotes title="Drew's Favorite Quotes" />
+  <App />
 </ApolloProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
