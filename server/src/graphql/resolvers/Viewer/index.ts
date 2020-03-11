af4cab61e794d9546f59bbe1c51d1e6a4d088110
Drew Page <drew@siegemedia.com>
2020-03-11 13:50:42 -0700

    added LogInViaGoogle function

--- server/src/graphql/resolvers/Viewer/index.ts
+++ server/src/graphql/resolvers/Viewer/index.ts
@@ -4,6 +4,29 @@ import { LogInArgs } from './types';
 import { Google } from '../../../lib/api';
 import crypto from 'crypto';
 
+const LogInViaGoogle = (
+  code: string, 
+  token: string, 
+  db: Database
+): Promise<User | undefined> => {
+  const { user } = await Google.logIn(code);
+
+  if (!user) {
+    throw new Error(`Failed to log in with Google`);
+  }
+
+  const userNamesList = user.names && user.names.length ? user.names : null;
+  const userPhotosList = user.photos && user.photos.length ? user.photos : null;
+  const userEmailList = user.emailAddresses && user.emailAddresses.length ? user.emailAddresses : null;
+
+  const userNames = userNamesList ? userNamesList[0].displayName : null;
+  const userId = userNamesList && userNamesList[0].metadata && userNamesList[0].metadata.source 
+    ? userNamesList[0].metadata.source.id 
+    : null;
+  const userAvatar = userPhotosList && userPhotosList[0].url ? userPhotosList[0].url : null;
+  const userEmail = userEmailList && userEmailList[0].value ? userEmailList[0].value : null;
+}
+
 export const viewerResolvers: IResolvers = {
   Query: {
     authUrl: (): string => {
