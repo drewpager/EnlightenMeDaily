9c8655e3fc68f025549f0968ea364fa9f91450ef
Drew Page <drew@siegemedia.com>
2020-03-13 06:56:48 -0700

    reduced branch packages

--- client/src/lib/types.ts
+++
@@ -1,6 +0,0 @@
-export interface Viewer {
-  _id: string | null;
-  token: string | null;
-  avatar: string | null;
-  didRequest: boolean;
-}
--- client/src/sections/Quote/Quote.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Quote = () => {
-  return <h2>Single Quote Page</h2>;
-}
