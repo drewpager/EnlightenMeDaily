64d7f948ac13d1e5e89ef1ca6d5604c6feafa93b
Drew Page <drew@siegemedia.com>
2020-03-15 09:36:40 -0700

    updating unstaged changes

---
+++ server/src/lib/type.ts
@@ -0,0 +1,45 @@
+import { Collection, ObjectId } from 'mongodb';
+
+export enum QuoteType {
+  Quote = "QUOTE",
+  Passage = "PASSAGE" 
+}
+export interface Quote {
+  _id: ObjectId;
+  quote: String;
+  author: String;
+  category: String;
+  period: Number;
+  image: String;
+  type: QuoteType;
+}
+
+export interface User {
+  _id: string;
+  token: string;
+  name: string;
+  avatar: string;
+  contact: string;
+  bookmarkings: ObjectId[];
+  quotes: ObjectId[];
+  authorized?: boolean;
+}
+
+export interface Viewer {
+  _id?: string;
+  token?: string;
+  avatar?: string;
+  didRequest: boolean;
+}
+
+export interface Bookmarking {
+  _id: ObjectId;
+  quote: ObjectId;
+  user: string;
+}
+
+export interface Database {
+  bookmarkings: Collection<Bookmarking>;
+  quotes: Collection<Quote>;
+  users: Collection<User>;
+}
---
+++ src/client/precache-manifest.292e257afd7b85a65cfd949c089355f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aefd31908499cd89a8ba7205d6d5e04e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ccef5ff12dcec972b83",
+    "url": "/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "f1b581e2adac845cf5b7",
+    "url": "/static/css/main.d51beeb9.chunk.css"
+  },
+  {
+    "revision": "5ccef5ff12dcec972b83",
+    "url": "/static/js/2.145f2514.chunk.js"
+  },
+  {
+    "revision": "91bcc555fc9d257ab7cac84356530039",
+    "url": "/static/js/2.145f2514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b581e2adac845cf5b7",
+    "url": "/static/js/main.29b695b5.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
