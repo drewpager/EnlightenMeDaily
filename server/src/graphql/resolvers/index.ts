9cce7ff9044bd48d4f7e52f99b6b4b52192c5d3d
Drew Page <drew@siegemedia.com>
2020-03-22 09:25:34 -0700

    restructured folder

--- server/src/graphql/resolvers/index.ts
+++
@@ -1,5 +0,0 @@
-import merge from 'lodash.merge';
-import { viewerResolvers } from './Viewer';
-import { userResolvers } from './User';
-
-export const resolvers = merge(viewerResolvers, userResolvers);
