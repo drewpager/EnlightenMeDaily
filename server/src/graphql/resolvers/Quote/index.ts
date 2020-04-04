a9b99db18b2b86942ae558dccdd189998ad6cb35
Drew Page <drew@siegemedia.com>
2020-04-04 07:23:36 -0700

    added mongodb's   to sort quotes based on search query

--- server/src/graphql/resolvers/Quote/index.ts
+++ server/src/graphql/resolvers/Quote/index.ts
@@ -12,51 +12,30 @@ export const quoteResolvers: IResolvers = {
       { category, filter, limit, page }: QuotesArgs,
       { db }: { db: Database }
     ): Promise<QuotesData> => {
-      const query: string = "";
+      //category here is the users search query
+      category;
       try {
         const data: QuotesData = {
           total: 0,
           result: []
         }
-        //playing around with if statement for user searching category
-        // remove if else blocks 
-        
-        if (query === category) {
-          let cursor = await db.quotes.find({query});
-          if (filter && filter === "OLDEST") {
-            cursor = cursor.sort({ period: -1 });
-          }
-  
-          if (filter && filter === "MOST_RECENT") {
-            cursor = cursor.sort({ period: 1 });
-          }
-  
-          cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0);
-          cursor = cursor.limit(limit);
-  
-          data.total = await cursor.count();
-          data.result = await cursor.toArray();
-  
-          return data;
-        } else {
-          let cursor = await db.quotes.find({});
-
-          if (filter && filter === "OLDEST") {
-            cursor = cursor.sort({ period: -1 });
-          }
-  
-          if (filter && filter === "MOST_RECENT") {
-            cursor = cursor.sort({ period: 1 });
-          }
-  
-          cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0);
-          cursor = cursor.limit(limit);
-  
-          data.total = await cursor.count();
-          data.result = await cursor.toArray();
-  
-          return data;
+      
+        let cursor = await db.quotes.find({ $text: { $search: `${category}` }});
+        if (filter && filter === "OLDEST") {
+          cursor = cursor.sort({ period: -1 });
         }
+
+        if (filter && filter === "MOST_RECENT") {
+          cursor = cursor.sort({ period: 1 });
+        }
+
+        cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0);
+        cursor = cursor.limit(limit);
+
+        data.total = await cursor.count();
+        data.result = await cursor.toArray();
+
+        return data;
       } catch (error) {
         throw new Error(`Failed to query quotes: ${error}`);
       }
