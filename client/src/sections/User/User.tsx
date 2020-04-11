678a980482cb3033aca2eb7221da7feb8f719c09
Drew Page <drew@siegemedia.com>
2020-04-11 07:18:44 -0700

    created a user page under construction view with button

--- client/src/sections/User/User.tsx
+++ client/src/sections/User/User.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Layout, Typography, Result, Button } from 'antd';
+import { UserAddOutlined, ToolOutlined } from '@ant-design/icons';
+
+const { Content } = Layout;
+const { Title } = Typography;
 
 export const User = () => {
-  return <h2>User Page</h2>;
+  return (
+    <Content> 
+      <Result
+        title="User Page Under Construction, We will notify you when this is ready."
+        extra={
+          <Link to="/"><Button type="primary">Check Out Some Quotes</Button></Link>
+        }
+      />
+    </Content>
+  );
 }
