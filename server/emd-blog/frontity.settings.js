7d8714e9dcf6031a2b7b2678cdad58049d53e22e
Drew Page <drew@siegemedia.com>
2020-05-04 06:19:53 -0700

    added frontity package and initial project

---
+++ server/emd-blog/frontity.settings.js
@@ -0,0 +1,57 @@
+const settings = {
+  "name": "emd-blog",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.io",
+      "title": "The Daily Dose",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "mobile",
+              "/category/mobile/"
+            ],
+            [
+              "Travel",
+              "/category/travel/"
+            ],
+            [
+              "KC",
+              "/author/kevin/"
+            ],
+            [
+              "About Us",
+              "/about-us/"
+            ]
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "api": "https://clevertap.com/wp-json"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
