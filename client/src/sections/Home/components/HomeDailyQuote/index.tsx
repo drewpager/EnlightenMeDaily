aa28b73649a632d2a7d4c7db259c79538cc8d14f
Drew Page <drew@siegemedia.com>
2020-05-08 07:19:28 -0700

    not sure

--- client/src/sections/Home/components/HomeDailyQuote/index.tsx
+++ client/src/sections/Home/components/HomeDailyQuote/index.tsx
@@ -1,14 +1,9 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
 import { QUOTES } from '../../../../lib/graphql/queries/Quotes';
 import { Quotes as QuotesData, QuotesVariables } from '../../../../lib/graphql/queries/Quotes/__generated__/Quotes';
 import { QuoteFilter } from '../../../../lib/graphql/globalTypes';
-// import { Quote } from '../../../../lib/graphql/queries/Quote/__generated__/Quote';
-import { Typography, Layout, Spin } from 'antd';
-
-const { Title } = Typography;
-const { Content } = Layout;
+import { Spin } from 'antd';
 
 interface QuoteProps {
   quote: string;
@@ -18,12 +13,13 @@ interface QuoteProps {
 
 const initialQuote = {
   quote: "Leadership is not about a title or a designation. It's about impact, influence and inspiration. Impact involves getting results, influence is about spreading the passion you have for your work, and you have to inspire teammates and customers.",
-  author: 'Robin S. Sharma',
-  id: '5e91f357e50e513beb89fc27'
+  author: "Robin S. Sharma",
+  id: "5e91f357e50e513beb89fc27"
 }
 
+
 export const HomeDailyQuote = () => {
-  const [DailyQuote, setDailyQuote] = useState<QuoteProps>(initialQuote);
+  const [DailyQuote, setDailyQuote] = useState<QuoteProps>();
   const { error, loading, data } = useQuery<QuotesData, QuotesVariables>(QUOTES, {
     variables: {
       category: "leadership, life, motivation",
@@ -41,29 +37,16 @@ export const HomeDailyQuote = () => {
       return result;
     }
     
-    let item;
-    
-    for (item in data.quotes) {
+    const getRandomValue = () => {
       const count: number = data.quotes.total;
-      const random: number = getRandomNumber(0, count + 1);
-      if (random > -1 && random < 366) { continue; }
-      const initTime: Date = new Date();
-      const localTime: number = initTime.getTimezoneOffset() * 60000;
-      const intNow: number = initTime.getTime() - localTime;
-      const intDay: number = Math.floor(intNow / 86400000);
-      const quoteIndex: number = intDay % random;
-
-      console.log('QuoteIndex: ', quoteIndex);
-      
-      if (quoteIndex && quoteIndex >= 0) {
-        const { quote, author, id } = data.quotes.result[0];
-        // let quote = data.quotes.result[0].quote;
-        // let author = data.quotes.result[0].author;
-        // let id = data.quotes.result[0].id;
-        console.log({quote, author, id});
-        setDailyQuote({quote, author, id});
-      }
+      const rand = getRandomNumber(0, count);
+      const { quote, author, id } = data.quotes.result[rand];
+      console.log("Inside getRandomValue: ", {quote, author, id});
+      setDailyQuote({quote, author, id});
+      return;  
     }
+
+    setInterval(getRandomValue, 86400000);
 
     if (loading) {
       return <Spin size="small" tip="loading quote of the day" />;
@@ -74,6 +57,7 @@ export const HomeDailyQuote = () => {
     }
   
     if (DailyQuote && DailyQuote.quote) {
+      // getRandomValue();
       return (
         <div>
           <div className="daily-quote__title">
